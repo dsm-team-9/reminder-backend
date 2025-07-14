8fdd198f9020c35eef183de81a2109e49a0a5d43
도경 <dokyungkim2926@gmail.com>
2025-07-14 18:58:25 +0900

    setting

---
+++ src/main/java/reminder/reminderbe/ReminderBeApplication.java
@@ -0,0 +1,13 @@
+package reminder.reminderbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReminderBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReminderBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/reminder/reminderbe/ReminderBeApplicationTests.java
@@ -0,0 +1,13 @@
+package reminder.reminderbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReminderBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
